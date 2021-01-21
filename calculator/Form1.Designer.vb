1b045cb3b83932e7e914edc679dc3c639309cc73
1-creator <vale johns@valegjohns.gmail.com>
2021-01-21 08:35:41 +0300

    first commit

---
+++ calculator/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxnum1 = New System.Windows.Forms.TextBox()
+        Me.TextBoxnum2 = New System.Windows.Forms.TextBox()
+        Me.Labelanswer = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(300, 73)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(295, 142)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'TextBoxnum1
+        '
+        Me.TextBoxnum1.Location = New System.Drawing.Point(437, 63)
+        Me.TextBoxnum1.Name = "TextBoxnum1"
+        Me.TextBoxnum1.Size = New System.Drawing.Size(76, 20)
+        Me.TextBoxnum1.TabIndex = 2
+        '
+        'TextBoxnum2
+        '
+        Me.TextBoxnum2.Location = New System.Drawing.Point(429, 119)
+        Me.TextBoxnum2.Name = "TextBoxnum2"
+        Me.TextBoxnum2.Size = New System.Drawing.Size(83, 20)
+        Me.TextBoxnum2.TabIndex = 3
+        '
+        'Labelanswer
+        '
+        Me.Labelanswer.AutoSize = True
+        Me.Labelanswer.Location = New System.Drawing.Point(364, 202)
+        Me.Labelanswer.Name = "Labelanswer"
+        Me.Labelanswer.Size = New System.Drawing.Size(33, 13)
+        Me.Labelanswer.TabIndex = 4
+        Me.Labelanswer.Text = "Label"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(150, 278)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "+"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(298, 278)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(60, 30)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "-"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(402, 278)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(54, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "*"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(506, 277)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "/"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(708, 379)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Labelanswer)
+        Me.Controls.Add(Me.TextBoxnum2)
+        Me.Controls.Add(Me.TextBoxnum1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxnum1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxnum2 As System.Windows.Forms.TextBox
+    Friend WithEvents Labelanswer As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+
+End Class
---
+++ calculator/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Dim Num1 As Single
+    Dim Num2 As Single
+    Dim Answer As Single
+   
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Num1 = Val(TextBoxnum1.Text)
+        Num2 = Val(TextBoxnum2.Text)
+        Answer = Num1 - Num2
+        Labelanswer.Text = Str(Answer)
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Num1 = Val(TextBoxnum1.Text)
+        Num2 = Val(TextBoxnum2.Text)
+        Answer = Num1 / Num2
+        Labelanswer.Text = Str(Answer)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Num1 = Val(TextBoxnum1.Text)
+        Num2 = Val(TextBoxnum2.Text)
+        Answer = Num1 + Num2
+        Labelanswer.Text = Str(Answer)
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Num1 = Val(TextBoxnum1.Text)
+        Num2 = Val(TextBoxnum2.Text)
+        Answer = Num1 * Num2
+        Labelanswer.Text = Str(Answer)
+    End Sub
+End Class
